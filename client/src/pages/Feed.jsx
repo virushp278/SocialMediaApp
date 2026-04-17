ab56210f2a738c6bc88986bd189cfe5472a4afe6
virushp278 <virushp278@gmail.com>
2026-04-17 17:39:43 +0530

    changes"

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -3,6 +3,7 @@ import { dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
 import PostCard from '../components/PostCard'
+import {assets} from '../assets/assets'
 
 const Feed = () => {
 
@@ -28,9 +29,12 @@ const Feed = () => {
        </div>
       </div>
       {/*right sidbbar */}
-      <div>
-        <div>
-          <h1>Sponsored</h1>
+      <div className='max-xl:hidden sticky top-0'>
+        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+          <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+          <img src={assets.sponsored_img} className="w-75 h-50 rounded-md" alt="" />
+          <p className='text-slate-600'>Email Marketing</p>
+          <p className='text-slate-400'>Supercharge your marketing with a powerful, easy to use platform built for results. </p>
         </div>
         <h1>Recent Messages</h1>
       </div>
