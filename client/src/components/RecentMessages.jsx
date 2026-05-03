32a5882ddb4972705a21db4fa1c74332918c12e7
virushp278 <virushp278@gmail.com>
2026-05-04 01:01:31 +0530

    feed page completed

--- client/src/components/RecentMessages.jsx
+++ client/src/components/RecentMessages.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { dummyRecentMessagesData } from '../assets/assets'
 import { Link } from 'react-router-dom'
-
+import moment from 'moment'
 const RecentMessages = () => {
 
     const [messages, setMessages
@@ -21,10 +21,20 @@ const RecentMessages = () => {
             <h3 className='font-semibold text-slate-8 mb-4'> Recent Messages</h3>
             <div className='flex flex-col max-h-56 overflow-y-scroll no-scrollbar'>
                 {
-                    messages.map((message,index)=>(
-                        <Link key={index} className='flex items-start gap-2 py-2 hover:bg-slate-100'>
+                    messages.map((message, index) => (
+                        <Link to={`/messages/$(message.from_user_id._id)`} key={index} className='flex items-start gap-2 py-2 hover:bg-slate-100'>
 
-                            <img src={message.from_user_id.profile_picture} alt=""/>
+                            <img src={message.from_user_id.profile_picture} alt="" className='w-8 h-8 rounded-full' />
+                            <div className='w-full'>
+                                <div className='flex justify-between'>
+                                    <p className='font-medium'>{message.from_user_id.full_name}</p>
+                                    <p className='text-[10px] text-slate-400'>{moment(message.createdAt).fromNow()}</p>
+                                </div>
+                                <div className='flex justify-between'>
+                                    <p className="text-gray-500">{message.text ? message.text:'Media'}</p>
+                                    {!message.seen && <p className="bg-inidgo-500 text-white w-4 h-4 flex items-center justify-center rounded-full text-[10px]">1</p>}
+                                </div>
+                            </div>
 
                         </Link>
                     ))
--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -4,6 +4,7 @@ import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
 import PostCard from '../components/PostCard'
 import {assets} from '../assets/assets'
+import RecentMessages from '../components/RecentMessages'
 
 const Feed = () => {
 
@@ -36,7 +37,7 @@ const Feed = () => {
           <p className='text-slate-600'>Email Marketing</p>
           <p className='text-slate-400'>Supercharge your marketing with a powerful, easy to use platform built for results. </p>
         </div>
-        <h1>Recent Messages</h1>
+        <RecentMessages/>
       </div>
     </div>
   ) : <Loading />
