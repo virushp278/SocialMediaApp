ceb8a51ef4c9b219aa4bf1a5861d78f8cb14e248
virushp278 <virushp278@gmail.com>
2026-06-09 00:54:24 +0530

    fixes

--- server/models/User.js
+++ server/models/User.js
@@ -17,6 +17,6 @@ const userSchema = new mongoose.Schema({
 
 },{timestamps:true, minimize:false})
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
